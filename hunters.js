9651dba655ba2d00fa804a0e524b70e37f1209e1
WeirdoCat <cykelmygmaerkeligmjc@gmail.com>
2020-02-23 14:04:32 +0100

    Spillet bliver nu progressivt sværere jo længere der går

--- hunters.js
+++ hunters.js
@@ -12,7 +12,7 @@ class Hunter {
         this.right = false;
         this.left = false;
         this.health = 10;
-
+        this.timer = 0;
 
     }
 
@@ -20,15 +20,16 @@ class Hunter {
 
     // Display the Rabbit
     show() {
-        this.x = mouseX;
-        this.y = mouseY;
+        this.x = mouseX-this.w;
+        this.y = mouseY-this.w;
         fill(255, 100);
         stroke(255);
         rect(this.x, this.y, this.w * 2, this.w * 2);
         fill(255*(10/this.health),255*(this.health/10),0);
         rect(this.x, this.y - 25, this.health, 2);
 
-        this.health -= 1 / 60;
+        this.health -= 1*(this.timer/(60*7)) / 60;
+        this.timer++;
     }
 
     move() {
